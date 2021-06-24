684cebf5a236d3fce3dcf8c4dad8a3fd5a7cf1b4
Sergey L <yo-suke@mail.ru>
2021-06-24 10:24:09 +0300

    fix badge-coverage 3

--- src/findDiff.js
+++ src/findDiff.js
@@ -2,28 +2,27 @@ import _ from 'lodash';
 
 const findDiff = (dataOne, dataTwo) => {
   const commonKeys = _.union(_.keys(dataOne), _.keys(dataTwo));
-  const result = commonKeys
-    .map((node) => {
-      if (!_.has(dataOne, node)) {
-        return { name: node, type: 'added', value: dataTwo[node] };
-      }
-      if (!_.has(dataTwo, node)) {
-        return { name: node, type: 'removed', value: dataOne[node] };
-      }
-      if (_.isObject(dataOne[node]) && _.isObject(dataTwo[node])) {
-        return { name: node, type: 'nested', children: findDiff(dataOne[node], dataTwo[node]) };
-      }
-      if ((typeof dataOne[node] !== typeof dataTwo[node])
+  const result = commonKeys.map((node) => {
+    if (!_.has(dataOne, node)) {
+      return { name: node, type: 'added', value: dataTwo[node] };
+    }
+    if (!_.has(dataTwo, node)) {
+      return { name: node, type: 'removed', value: dataOne[node] };
+    }
+    if (_.isObject(dataOne[node]) && _.isObject(dataTwo[node])) {
+      return { name: node, type: 'nested', children: findDiff(dataOne[node], dataTwo[node]) };
+    }
+    if ((typeof dataOne[node] !== typeof dataTwo[node])
       || (dataOne[node] !== dataTwo[node])) {
-        return {
-          name: node,
-          type: 'changed',
-          valueBefore: dataOne[node],
-          valueAfter: dataTwo[node],
-        };
-      }
-      return { name: node, type: 'unchanged', value: dataOne[node] };
-    });
+      return {
+        name: node,
+        type: 'changed',
+        valueBefore: dataOne[node],
+        valueAfter: dataTwo[node],
+      };
+    }
+    return { name: node, type: 'unchanged', value: dataOne[node] };
+  });
   return _.sortBy(result, 'name');
 };
 
