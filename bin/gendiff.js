9da97b7c5ea022abcd4f53f9c34512390e0984a7
Sergey L <yo-suke@mail.ru>
2021-06-28 12:40:23 +0300

    --fix output-format in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'output format', 'tree')
+  .option('-f, --format [type]', 'output format', 'plain/stylish/json')
   .action((firstConfig, secondConfig) => (
     console.log(genDiff(firstConfig, secondConfig, program.opts().format))))
   .parse(process.argv);
