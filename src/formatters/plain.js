60f73dd016837bb709a1af60f15f860b3746ec3a
Sergey L <yo-suke@mail.ru>
2021-06-28 19:38:57 +0300

    --fix formatters/plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,35 +1,30 @@
 import _ from 'lodash';
 
-const getNodeName = (node, ancestor) => {
-  if (ancestor === '') {
-    return `${node.name}`;
-  }
-  return `${ancestor}.${node.name}`;
-};
+const getNodeName = (node, path) => (path ? `${path}.${node.name}` : node.name);
 
 const getValue = (value) => {
-  if (_.isObject(value)) {
-    return '[complex value]';
+  if (value === null) {
+    return 'null';
   }
   if (typeof value === 'string') {
     return `'${value}'`;
   }
-  return value;
+  return !_.isObject(value) ? value : '[complex value]';
 };
 
-const formatPlain = (diff, ancestor = '') => {
+const formatPlain = (diff, depth = '') => {
   const result = diff
     .filter((node) => node.type !== 'unchanged')
     .map((node) => {
       switch (node.type) {
         case 'removed':
-          return `Property '${getNodeName(node, ancestor)}' was removed`;
+          return `Property '${getNodeName(node, depth)}' was removed`;
         case 'changed':
-          return `Property '${getNodeName(node, ancestor)}' was updated. From ${getValue(node.value1)} to ${getValue(node.value2)}`;
+          return `Property '${getNodeName(node, depth)}' was updated. From ${getValue(node.value1)} to ${getValue(node.value2)}`;
         case 'added':
-          return `Property '${getNodeName(node, ancestor)}' was added with value: ${getValue(node.value)}`;
+          return `Property '${getNodeName(node, depth)}' was added with value: ${getValue(node.value)}`;
         case 'nested':
-          return formatPlain(node.children, getNodeName(node, ancestor));
+          return formatPlain(node.children, getNodeName(node, depth));
         default:
           throw new Error('Unknown node status');
       }
