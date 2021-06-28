0555ba5d8a3caca5f913ce8bbbdf8e6fd36c67ed
Sergey L <yo-suke@mail.ru>
2021-06-28 19:54:17 +0300

    --fix2 formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,22 +1,24 @@
 import _ from 'lodash';
 
-const makeTabs = (spaces) => ('    '.repeat(spaces));
+const makeTabs = (level) => ('    '.repeat(level));
 
-const makeString = (value, spaces = 0) => {
+const stringify = (value, spaces = 0) => {
   if (!_.isObject(value)) {
     return value;
   }
-  const lines = _.keys(value).map((node) => `${makeTabs(spaces)}    ${node}: ${makeString(value[node], spaces + 1)}`);
+  const lines = _.keys(value).map((node) => `${makeTabs(spaces)}    ${node}: ${stringify(value[node], spaces + 1)}`);
   const innerValue = lines.join('\n');
   return `{\n${innerValue}\n${makeTabs(spaces)}}`;
 };
 
 const stylishFormater = (diff, spaces = 0) => {
+  const tabs = makeTabs(spaces);
+  const getStrings = (val) => stringify(val, spaces + 1);
   const lines = diff.map((node) => {
-    const buildLine = (char, value) => `${makeTabs(spaces)}  ${char} ${node.name}: ${makeString(value, spaces + 1)}`;
+    const buildLine = (char, value) => `${tabs}  ${char} ${node.name}: ${getStrings(value)}`;
     switch (node.type) {
       case 'changed':
-        return `${makeTabs(spaces)}  - ${node.name}: ${makeString(node.value1, spaces + 1)}\n ${makeTabs(spaces)} + ${node.name}: ${makeString(node.value2, spaces + 1)}`;
+        return `${tabs}  - ${node.name}: ${getStrings(node.value1)}\n ${tabs} + ${node.name}: ${getStrings(node.value2)}`;
       case 'unchanged':
         return buildLine(' ', node.value);
       case 'added':
@@ -24,13 +26,13 @@ const stylishFormater = (diff, spaces = 0) => {
       case 'removed':
         return buildLine('-', node.value);
       case 'nested':
-        return `${makeTabs(spaces)}    ${node.name}: ${stylishFormater(node.children, spaces + 1)}`;
+        return `${tabs}    ${node.name}: ${stylishFormater(node.children, spaces + 1)}`;
       default:
         throw new Error(`${node.type} is wrong type`);
     }
   });
   const innerValue = lines.join('\n');
-  return `{\n${innerValue}\n${makeTabs(spaces)}}`;
+  return `{\n${innerValue}\n${tabs}}`;
 };
 
 export default stylishFormater;
