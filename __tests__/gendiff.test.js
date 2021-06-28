2006d3be790abd3881ea9e37dcef517a655363ea
Sergey L <yo-suke@mail.ru>
2021-06-28 12:35:19 +0300

    --fix2 tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,40 +3,21 @@ import fs from 'fs';
 import { cwd } from 'process';
 import genDiff from '../src/index.js';
 
-const plainResult = fs
-  .readFileSync(path.resolve(cwd(), '__tests__/__fixtures__', 'plain.diff'), 'utf8');
-const recurciveResult = fs
-  .readFileSync(path.resolve(cwd(), '__tests__/__fixtures__', 'recurciveResult.diff'), 'utf8');
-const jsonResult = fs
-  .readFileSync(path.resolve(cwd(), '__tests__/__fixtures__', 'json.diff'), 'utf8');
+const getResultFile = (fileName) => fs
+  .readFileSync(path.resolve(cwd(), '__tests__/__fixtures__', fileName), 'utf8');
 
-// const plainResult = fs
-//   .readFileSync(path.resolve(__dirname, './__fixtures__/plain.diff'), 'utf8');
-// const recurciveResult = fs
-//   .readFileSync(path.resolve(__dirname, './__fixtures__/recurciveResult.diff'), 'utf8');
-// const jsonResult = fs
-//   .readFileSync(path.resolve(__dirname, './__fixtures__/json.diff'), 'utf8');
-
-test('genDiff JSON plain', () => {
-  expect(genDiff('file1.json', 'file2.json', 'plain')).toEqual(plainResult);
-});
-
-test('genDiff YML plain', () => {
-  expect(genDiff('file1.yml', 'file2.yml', 'plain')).toEqual(plainResult);
-});
-
-test('genDiff JSON nested', () => {
-  expect(genDiff('file1.json', 'file2.json', 'stylish')).toEqual(recurciveResult);
-});
-
-test('genDiff YML nested', () => {
-  expect(genDiff('file1.yml', 'file2.yml', 'stylish')).toEqual(recurciveResult);
-});
-
-test('genDiff JSON json', () => {
-  expect(genDiff('file1.json', 'file2.json', 'json')).toEqual(jsonResult);
-});
-
-test('genDiff YML json', () => {
-  expect(genDiff('file1.yml', 'file2.yml', 'json')).toEqual(jsonResult);
+describe('genDiff', () => {
+  const stylishDiff = genDiff('file1.json', 'file2.json', 'stylish');
+  const plainDiff = genDiff('file1.json', 'file2.json', 'plain');
+  const jsonDiff = genDiff('file1.json', 'file2.json', 'json');
+  const stylishResult = getResultFile('stylishResult.diff');
+  const plainResult = getResultFile('plainResult.diff');
+  const jsonResult = getResultFile('jsonResult.diff');
+  test.each([
+    [stylishResult, stylishDiff],
+    [plainResult, plainDiff],
+    [jsonResult, jsonDiff],
+  ])('compare two JSON and yaml files', (a, expected) => {
+    expect(a).toBe(expected);
+  });
 });
