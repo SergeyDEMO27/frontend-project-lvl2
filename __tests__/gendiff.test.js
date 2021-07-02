a6ad9739293100ad6cc95faada94e9afeca728e4
Sergey L <yo-suke@mail.ru>
2021-07-02 12:42:39 +0300

    --fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,21 +3,23 @@ import fs from 'fs';
 import { cwd } from 'process';
 import genDiff from '../src/index.js';
 
+const fileFormats = ['json', 'yml'];
+const resultFormats = ['stylish', 'plain', 'json'];
+const fixtures = {};
 const getFixturePath = (fileName) => path.resolve(cwd(), '__tests__/__fixtures__', fileName);
 const readFile = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf8');
 
-describe('genDiff', () => {
-  const stylishDiff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const plainDiff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
-  const jsonDiff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
-  const stylishResult = readFile('stylishResult.diff');
-  const plainResult = readFile('plainResult.diff');
-  const jsonResult = readFile('jsonResult.diff');
-  test.each([
-    [stylishResult, stylishDiff],
-    [plainResult, plainDiff],
-    [jsonResult, jsonDiff],
-  ])('compare two JSON and yaml files', (a, expected) => {
-    expect(a).toBe(expected);
+beforeAll(() => {
+  resultFormats.forEach((format) => {
+    fixtures[format] = readFile(`result_${format}.txt`);
   });
+});
+
+test.each(fileFormats)('compare two JSON and yaml files', (format) => {
+  const file1Path = getFixturePath(`file1.${format}`);
+  const file2Path = getFixturePath(`file2.${format}`);
+
+  expect(genDiff(file1Path, file2Path)).toBe(fixtures.stylish);
+  expect(genDiff(file1Path, file2Path, 'plain')).toBe(fixtures.plain);
+  expect(genDiff(file1Path, file2Path, 'json')).toBe(fixtures.json);
 });
