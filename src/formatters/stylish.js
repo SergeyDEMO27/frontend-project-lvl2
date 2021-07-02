292695afbbf8084e93d57f8d6df90fde1ad10367
Sergey L <yo-suke@mail.ru>
2021-07-02 11:27:32 +0300

    --fix formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,18 +2,18 @@ import _ from 'lodash';
 
 const makeTabs = (level) => ('    '.repeat(level));
 
-const stringify = (value, spaces = 0) => {
+const stringify = (value, level = 0) => {
   if (!_.isObject(value)) {
     return value;
   }
-  const lines = _.keys(value).map((node) => `${makeTabs(spaces)}    ${node}: ${stringify(value[node], spaces + 1)}`);
+  const lines = _.keys(value).map((node) => `${makeTabs(level)}    ${node}: ${stringify(value[node], level + 1)}`);
   const innerValue = lines.join('\n');
-  return `{\n${innerValue}\n${makeTabs(spaces)}}`;
+  return `{\n${innerValue}\n${makeTabs(level)}}`;
 };
 
-const stylishFormater = (diff, spaces = 0) => {
-  const tabs = makeTabs(spaces);
-  const getStrings = (val) => stringify(val, spaces + 1);
+const stylishFormater = (diff, level = 0) => {
+  const tabs = makeTabs(level);
+  const getStrings = (val) => stringify(val, level + 1);
   const lines = diff.map((node) => {
     const buildLine = (char, value) => `${tabs}  ${char} ${node.name}: ${getStrings(value)}`;
     switch (node.type) {
@@ -26,7 +26,7 @@ const stylishFormater = (diff, spaces = 0) => {
       case 'removed':
         return buildLine('-', node.value);
       case 'nested':
-        return `${tabs}    ${node.name}: ${stylishFormater(node.children, spaces + 1)}`;
+        return `${tabs}    ${node.name}: ${stylishFormater(node.children, level + 1)}`;
       default:
         throw new Error(`${node.type} is wrong type`);
     }
