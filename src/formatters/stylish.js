1247bbe27fc8cbc2d981744b176e150252146e14
Sergey L <yo-suke@mail.ru>
2021-07-02 11:34:30 +0300

    --fix2 formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,24 +1,24 @@
 import _ from 'lodash';
 
-const makeTabs = (level) => ('    '.repeat(level));
+const makePrefix = (level) => ('    '.repeat(level));
 
 const stringify = (value, level = 0) => {
   if (!_.isObject(value)) {
     return value;
   }
-  const lines = _.keys(value).map((node) => `${makeTabs(level)}    ${node}: ${stringify(value[node], level + 1)}`);
+  const lines = _.keys(value).map((node) => `${makePrefix(level)}    ${node}: ${stringify(value[node], level + 1)}`);
   const innerValue = lines.join('\n');
-  return `{\n${innerValue}\n${makeTabs(level)}}`;
+  return `{\n${innerValue}\n${makePrefix(level)}}`;
 };
 
 const stylishFormater = (diff, level = 0) => {
-  const tabs = makeTabs(level);
+  const prefix = makePrefix(level);
   const getStrings = (val) => stringify(val, level + 1);
   const lines = diff.map((node) => {
-    const buildLine = (char, value) => `${tabs}  ${char} ${node.name}: ${getStrings(value)}`;
+    const buildLine = (char, value) => `${prefix}  ${char} ${node.name}: ${getStrings(value)}`;
     switch (node.type) {
       case 'changed':
-        return `${tabs}  - ${node.name}: ${getStrings(node.value1)}\n ${tabs} + ${node.name}: ${getStrings(node.value2)}`;
+        return `${prefix}  - ${node.name}: ${getStrings(node.value1)}\n ${prefix} + ${node.name}: ${getStrings(node.value2)}`;
       case 'unchanged':
         return buildLine(' ', node.value);
       case 'added':
@@ -26,13 +26,13 @@ const stylishFormater = (diff, level = 0) => {
       case 'removed':
         return buildLine('-', node.value);
       case 'nested':
-        return `${tabs}    ${node.name}: ${stylishFormater(node.children, level + 1)}`;
+        return `${prefix}    ${node.name}: ${stylishFormater(node.children, level + 1)}`;
       default:
         throw new Error(`${node.type} is wrong type`);
     }
   });
   const innerValue = lines.join('\n');
-  return `{\n${innerValue}\n${tabs}}`;
+  return `{\n${innerValue}\n${prefix}}`;
 };
 
 export default stylishFormater;
